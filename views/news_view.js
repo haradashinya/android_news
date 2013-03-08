43902691e32825fbe446b7097214c15689f3f3ea
okamurayasuyuki <haradashinya@gmail.com>
2013-03-08 17:50:37 +0900

    a

--- views/news_view.js
+++ views/news_view.js
@@ -2,7 +2,7 @@ var NewsView = function(em,data){
     var self = this;
     self.text = "";
     self.render = function(){
-        var d = $("#main-view").find("#content");
+        var d = $("#main-view").find(".content");
         self.text = $("<li>" + data["content"] + "</li>");
         d.append(self.text);
         return self;
