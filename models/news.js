f57669c78a7615bf24368358ee009052dcbbb4bf
okamurayasuyuki <haradashinya@gmail.com>
2013-03-08 14:47:31 +0900

    em

--- models/news.js
+++ models/news.js
@@ -3,7 +3,14 @@ var NewsModel = function(){
     var self = {
         name: "hello",
         currentNumber: 0,
+        items: []
     };
+    var em = new EventEmitter();
+    em.on("currentNumber",function(res){
+        console.log(res);
+        console.log("change");
+
+    });
     self.fetchLatest = function(callback){
         $.ajax({
             type: "GET",
@@ -18,19 +25,27 @@ var NewsModel = function(){
     };
 
 
+    // if setted currentNumber, then call fetchNews.
     self.onReceivedCurrentNumber = function(){
-        return fetchNewsFromNum();
+        return fetchNews();
+    };
+    self.onReceivedNews = function(data){
+        console.log("data");
+
+        console.log(data);
+
     };
 
 
 
-    var fetchNewsFromNum = function(){
+    var fetchNews = function(){
         $.ajax({
             type: "GET",
             url: "http://pickalize.info:3001/latest/"+ self.currentNumber,
             dataType: "json",
             success:function(data){
                 console.log(data);
+                em.emit("currentNumber");
 
             }
         });
