371f7c88bfc3d2062860598a7661321c85227c46
okamurayasuyuki <haradashinya@gmail.com>
2013-03-08 18:08:12 +0900

    fix

--- views/news_view.js
+++ views/news_view.js
@@ -1,10 +1,16 @@
 var NewsView = function(em,data){
     var self = this;
-    self.text = "";
+    self.template = "";
+
+
     self.render = function(){
         var d = $("#main-view").find(".content");
-        self.text = $("<li>" + data["content"] + "</li>");
-        d.append(self.text);
+        var opts = {
+            content: data["content"],
+            href: data["href"].replace('"','',"gim")
+        };
+        self.template = _.template('<li><a href="<%= href %>"><%= content %></a></li>',opts);
+        d.append(self.template);
         return self;
     };
     return self;
